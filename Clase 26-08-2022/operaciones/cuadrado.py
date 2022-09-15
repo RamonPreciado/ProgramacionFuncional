5bb5ef29ac21bbdf8157954a351258c6d1cfc0c8
RamonPreciado <111663869+RamonPreciado@users.noreply.github.com>
2022-09-15 00:18:37 -0500

    Create cuadrado.py

---
+++ Clase 26-08-2022/operaciones/cuadrado.py
@@ -0,0 +1,2 @@
+def cuadrado(a:int|float)->int|float: #Se pasa un argumento ya sea de tipo int o de tipo float y va retornar un valor int o float
+    return a*a #Se devuelve el resultado de la operacion
