669a42361916ce7cf09362a018bac5903b7fabe5
RamonPreciado <111663869+RamonPreciado@users.noreply.github.com>
2022-09-15 00:20:33 -0500

    Create operaciones.py

---
+++ Clase 26-08-2022/operaciones/operaciones.py
@@ -0,0 +1,12 @@
+def sumar(a:int|float, b:int|float)->int|float: #Se pasa un argumento ya sea de tipo int o de tipo float y va retornar un valor int o float
+    return a + b #Se devuelve el resultado de la operacion
+
+def restar(a:int|float, b:int|float)->int|float: 
+    return a- b 
+def multiplicar(a:int|float,b:int|float)->int|float: 
+    return a*b 
+
+def dividir(a:int|float, b:int|float)->int|float: 
+    return a/b 
+def cuadrado(a:int|float)->int|float: 
+    return a*a 
