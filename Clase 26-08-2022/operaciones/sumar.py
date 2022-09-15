549f80d3565d80182611b0c4c3ac00e509453c30
RamonPreciado <111663869+RamonPreciado@users.noreply.github.com>
2022-09-15 00:21:39 -0500

    Create sumar.py

---
+++ Clase 26-08-2022/operaciones/sumar.py
@@ -0,0 +1,2 @@
+def sumar(a:int|float, b:int|float) -> int|float: #Se pasa un argumento ya sea de tipo int o de tipo float y va retornar un valor int o float
+    return a + b #Se devuelve el resultado de la operacion
